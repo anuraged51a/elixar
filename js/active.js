7e4a3ecf3ac7f965fa04e9647a77c1b56d6e0b55
anuraged51a <anuraged51a@gmail.com>
2020-01-26 21:59:52 +0530

    26 Jan commit

---
+++ js/active.js
@@ -0,0 +1,29 @@
+ $(function () {
+     SyntaxHighlighter.all();
+ });
+ $(window).load(function () {
+     $('.flexslider').flexslider({
+         animation: "slide",
+         controlNav: "thumbnails",
+         start: function (slider) {
+             $('body').removeClass('loading');
+         }
+     });
+ });
+
+
+
+
+ $(".verticalCarousel").verticalCarousel({
+     currentItem: 1,
+     showItems: 4,
+
+ });
+
+ var icons = {
+     header: "ui-icon-circle-plus",
+     activeHeader: "ui-icon-circle-minus"
+ };
+ $("#accordion").accordion({
+     icons: icons
+ });
